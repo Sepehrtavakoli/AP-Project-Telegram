88ec2dc6b568114eaf0effe801229ebf904d4730
ParsaAhmadi <m.parsa.programmer@gmail.com>
2025-09-01 23:32:34 +0330

    login page kamel shod

--- src/main/java/org/example/approjectgui/TelegramApplication.java
+++ src/main/java/org/example/approjectgui/TelegramApplication.java
@@ -3,7 +3,7 @@ package org.example.approjectgui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.stage.*;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -11,13 +11,11 @@ public class TelegramApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(TelegramApplication.class.getResource("login.fxml"));
-        Scene loginScene = new Scene(fxmlLoader.load(), 620, 480);
-
-//        Image logo = new Image("images/Picture1.png");
-//        stage.getIcons().add(logo);
+        Scene loginScene = new Scene(fxmlLoader.load(), 360, 500); // اندازه جدید
 
         stage.setTitle("Telegram");
         stage.setScene(loginScene);
+        stage.setResizable(false); // غیرفعال کردن تغییر اندازه پنجره
         stage.show();
     }
 
